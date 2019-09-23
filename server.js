1e237de7a217a6491ac9153944bf787a12d2cda7
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-09-23 17:07:59 +0100

    initial setup for node/express api

---
+++ server.js
@@ -0,0 +1,12 @@
+// Import express
+let express = require('express')
+// Initialize the app
+let app = express();
+// Setup server port
+var port = process.env.PORT || 8080;
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
