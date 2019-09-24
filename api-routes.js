d31d5b0e301b6a716b7984d47d77914494293d55
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-09-25 00:21:35 +0100

    API docummentation

--- api-routes.js
+++ api-routes.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const userController = require('./controllers/userController.js');
 const itemsController = require('./controllers/itemsController.js');
 
-router.get('/', function (req, res) {
-    res.json({
-        status: 'Test',
-        message: 'Testing',
-    });
+router.get('/', function (req, res) 
+{
+	res.setHeader("Content-Type", "text/html");
+    res.end(`
+    	<p style="">Official docummentation for this API could be found at <br />
+    	<a href="https://jaycodist.github.io/muses-backend-docs" target="_self">
+    	https://jaycodist.github.io/muses-backend-docs</a><p>`);
 });
 // App routes
 router.route('/login').post(userController.login);
