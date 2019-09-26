ab582ad3d2ee303ac8370a3f0affe0e76f224f4e
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-09-26 02:39:07 +0100

    changed requests url to hosted api url

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const auth_token = "muse_nwc_auth_token";
 const user_name = "muse_nwc_name";
 const user_email = "muse_nwc_email";
-const backendUrl = "http://localhost:3000";
+//const backendUrl = "http://localhost:3000";	// For local testing
+const backendUrl = "https://muses-nwc-api.herokuapp.com";
 
 // redirect if logged in
 //if (window.localStorage.getItem(auth_token))
