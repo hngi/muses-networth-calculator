13c74b4039b7aa1be1cc411de47f435b08a31b1b
KingAbesh <abeshekwere@gmail.com>
2019-09-28 14:02:27 +0100

    conflict resolution

--- js/overview.js
+++ js/overview.js
@@ -1,51 +1,60 @@
-let dummyItems = 
-[
-	{
-		id: "sarstas23",
-		type: "ASSET",
-		description: "Mercedes car",
-		value: 3000000
-	},
-	{
-		id: "sarstas231",
-		type: "LIABILITY",
-		description: "Mercedes car",
-		value: -13000000
-	},
-	{
-		id: "sarstas2312",
-		type: "LIABILITY",
-		description: "Mercedes car",
-		value: -50000
-	},
-	{
-		id: "sarstas23454",
-		type: "LIABILITY",
-		description: "Mercedes car",
-		value: -3000000
-	},
-	{
-		id: "sarstas23ds",
-		type: "ASSET",
-		description: "Cattle Ranch",
-		value: 30000000
-	},
-	{
-		id: "sarstas23as",
-		type: "ASSET",
-		description: "House",
-		value: 3000000
-	},
-	{
-		id: "sarstas2398",
-		type: "ASSET",
-		description: "Mercedes car",
-		value: 3000000
-	},
-];
+auth_token = "muse_nwc_auth_token";
+overview_mode = "muse_nwc_overview_mode";
+item_edit = "muse_nwc_item_edit";
+const backendUrl = "https://muses-nwc-api.herokuapp.com";
 
+
+const table = document.querySelector("table");
+let items = [];
 let selectedItems = [];
 let displayedCBs = [];
+if (!localStorage.getItem(overview_mode)) 
+	localStorage.setItem(overview_mode, "ASSET");
+
+const powerUp = () =>
+{
+	let mode = localStorage.getItem(overview_mode);
+	fetch(backendUrl + "/api/items",
+	{
+		method: 'GET', // *GET, POST, PUT, DELETE, etc.
+		headers: 
+		{
+		  'Content-Type': 'application/json',
+		  'Origin': 'muse-client',
+		  'Authorization': 'Bearer ' + (localStorage.getItem(auth_token) || '')
+		},
+	})
+	.then(data => data.json())
+	.then(json => 
+	{
+		if (json.code)
+			console.log(`Error! ${json.code}: ${json.errors}`)
+		else
+		{
+			items = json;
+			populateTable(items);
+		}
+	});
+	document.getElementById("overview-info").style.display = "block";
+	document.getElementById("overview-info").textContent = "Items loading. Please wait. . .";
+
+	switch (mode)
+	{
+		case "ASSET":
+			document.getElementById("liabilities-switch").classList.remove("item-switch-active");
+			document.getElementById("assets-switch").classList.add("item-switch-active");
+			document.getElementById("overview-add-item").textContent = "Add Asset";
+			break;
+		case "LIABILITY":
+			document.getElementById("liabilities-switch").classList.add("item-switch-active");
+			document.getElementById("assets-switch").classList.remove("item-switch-active");
+			document.getElementById("overview-add-item").textContent = "Add Liability";
+			break;
+	}
+}
+
+
+powerUp();
 
 document.getElementById("overview-select-all").checked = false;
 
@@ -70,26 +79,49 @@ const selectRow = e =>
 	if (e.target != cb)
 		cb.checked = !cb.checked;
 	if (cb.checked)
-		selectedItems.push(dummyItems.find(a => cb.id == a.id));
+		selectedItems.push(items.find(a => cb.id == a.id));
 	else
 		selectedItems = selectedItems.filter(a => cb.id != a.id);
 	document.getElementById("overview-select-all").checked = false;
 	updateControlFocus();
 }
 
+const deleteItem = () =>
+{
+	document.getElementById("overview-modal-container").style.display = "none";
+	for (let i of selectedItems)
+	{
+		fetch(backendUrl + "/api/items",
+		{
+			method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+			headers: 
+			{
+			  'Content-Type': 'application/json',
+			  'Origin': 'muse-client',
+			  'Authorization': 'Bearer ' + (localStorage.getItem(auth_token) || '')
+			},
+			body: JSON.stringify({item_id: i.id}) // body data type must match "Content-Type" header
+		});
+	}
+
+	selectedItems = [];
+	updateControlFocus();
+	powerUp();
+}
+
 const populateTable = (items) =>
 {
+	let mode = localStorage.getItem(overview_mode);
 	selectedItems = [];
-	const table = document.querySelector("table");
+	displayedCBs = [];
 	let th = table.firstChild;
 	while (table.childNodes[2])
 		table.removeChild(table.childNodes[2]);
 
 	table.appendChild(th);
-	const mode = table.getAttribute("data-mode");
 	for (let item of items)
 	{
-		if (item.type !== mode)
+		if (item.type.toLowerCase() !== mode.toLowerCase())
 			continue;
 		let row = document.createElement("tr");
 		row.setAttribute("data-id", item.id);
@@ -101,7 +133,7 @@ const populateTable = (items) =>
 		displayedCBs.push(cb);
 
 		let type = document.createElement("td");
-		type.appendChild(document.createTextNode(item.type));
+		type.appendChild(document.createTextNode(item.type.toUpperCase()));
 
 		let desc = document.createElement("td");
 		desc.appendChild(document.createTextNode(item.description));
@@ -118,35 +150,40 @@ const populateTable = (items) =>
 
 		table.appendChild(row);
 	}
+	if (!displayedCBs.length)
+	{
+		document.getElementById("overview-info").style.display = "block";
+		document.getElementById("overview-info").textContent = `You have not added any ${mode === "ASSET" ? 
+			"assets" : "liabilities"}`;
+	}
+	else
+		document.getElementById("overview-info").style.display = "none";
 }
-
-
-
-populateTable(dummyItems);
-
 
 document.getElementById("assets-switch").addEventListener("click", e =>
 {
-	document.querySelector("table").setAttribute("data-mode", e.target.getAttribute("data-switch-value"));
+	localStorage.setItem(overview_mode, localStorage.getItem(overview_mode) === "ASSET" ?
+		"LIABILITY" : "ASSET");
 	e.target.classList.add("item-switch-active");
 	document.getElementById("liabilities-switch").classList.remove("item-switch-active");
 	document.getElementById("overview-add-item").textContent = "Add Asset";
 	document.getElementById("overview-select-all").checked = false;
 	selectedItems = [];
 	updateControlFocus();
-	populateTable(dummyItems);
+	populateTable(items);
 });
 
 document.getElementById("liabilities-switch").addEventListener("click", e =>
 {
-	document.querySelector("table").setAttribute("data-mode", e.target.getAttribute("data-switch-value"));
+	localStorage.setItem(overview_mode, localStorage.getItem(overview_mode) === "ASSET" ?
+		"LIABILITY" : "ASSET");
 	e.target.classList.add("item-switch-active");
 	document.getElementById("assets-switch").classList.remove("item-switch-active");
 	document.getElementById("overview-add-item").textContent = "Add Liability";
 	document.getElementById("overview-select-all").checked = false;
 	selectedItems = [];
 	updateControlFocus();
-	populateTable(dummyItems);
+	populateTable(items);
 });
 
 document.getElementById("overview-select-all").addEventListener("click", e =>
@@ -156,7 +193,7 @@ document.getElementById("overview-select-all").addEventListener("click", e =>
 	{
 		cb.checked = e.target.checked;
 		if (e.target.checked)
-			selectedItems.push(dummyItems.find(a => cb.id == a.id));
+			selectedItems.push(items.find(a => cb.id == a.id));
 	}
 	updateControlFocus();
 });
@@ -164,15 +201,38 @@ document.getElementById("overview-select-all").addEventListener("click", e =>
 document.getElementById("overview-controls-edit").addEventListener("click", e =>
 {
 	e.preventDefault();
-	
+	if (!selectedItems.length)
+		return;
+	localStorage.setItem(item_edit, JSON.stringify(selectedItems[0]));
+	location.href = "newitem.html";
 });
 
 document.getElementById("overview-controls-delete").addEventListener("click", e =>
 {
 	e.preventDefault();
+	if(!selectedItems.length)
+		return;
+
+	document.getElementById("overview-modal-container").style.display = "block";
+	document.getElementById("overview-delete-query").textContent = "Are you sure you want to delete " + 
+		(selectedItems.length > 1 ? "these items?" : "this item?");
+	document.getElementById("overview-delete-yes").addEventListener("click", e => 
+	{
+		deleteItem();
+	});
+	document.getElementById("overview-modal-container").addEventListener("click", e => 
+	{
+		if (e.path[0] == document.getElementById("overview-modal-container"))
+			document.getElementById("overview-modal-container").style.display = "none";
+	});
 });
 
 document.getElementById("overview-controls-sort").addEventListener("click", e =>
 {
 	e.preventDefault();
+});
+
+document.getElementById("overview-add-item").addEventListener("click", e =>
+{
+	location.href = "newitem.html";
 });
