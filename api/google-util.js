80e24e338fbe983b9d72ee20c3828e7ba21a1c21
ce0la <oshunboteo@gmail.com>
2019-10-04 08:54:59 +0100

    added the functionality although not working properly yet

---
+++ api/google-util.js
@@ -0,0 +1,51 @@
+// import { google } from 'googleapis';
+// // const google = require(googleapis);
+
+// const googleConfig = {
+//   clientId: '1039137931813-037lfes22v0goblregnhs39lvio8iqpj.apps.googleusercontent.com', // e.g. asdfghjkljhgfdsghjk.apps.googleusercontent.com
+//   clientSecret: '<GOOGLE_CLIENT_SECRET>', // e.g. _ASDFA%DFASDFASDFASD#FAD-
+//   redirect: 'http://muses-networth.netlify.com/google-auth' // this must match your google api settings
+// };
+
+// /**
+//  * Create the google auth object which gives us access to talk to google's apis.
+//  */
+// function createConnection() {
+//   return new google.auth.OAuth2(
+//     googleConfig.clientId,
+//     googleConfig.clientSecret,
+//     googleConfig.redirect
+//   );
+// }
+
+
+// /**
+//  * This scope tells google what information we want to request.
+//  */
+// const defaultScope = [
+//     'https://www.googleapis.com/auth/plus.me',
+//     'https://www.googleapis.com/auth/userinfo.email',
+//   ];
+  
+//   /**
+//    * Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
+//    */
+//   function getConnectionUrl(auth) {
+//     return auth.generateAuthUrl({
+//       access_type: 'offline',
+//       prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
+//       scope: defaultScope
+//     });
+//   }
+  
+//   /**
+//    * Create the google url to be sent to the client.
+//    */
+//   function urlGoogle() {
+//     const auth = createConnection(); // this is from previous step
+//     const url = getConnectionUrl(auth);
+//     return url;
+//   }
+
+  
+  
